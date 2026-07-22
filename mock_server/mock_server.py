3f1bba74c7f480b4ca5a96251b42dc9b28150034
fibusa <fedor.bykov.90@gmail.com>
2026-07-22 08:39:47 +0300

    Add mock server

--- mock_server/mock_server.py
+++ mock_server/mock_server.py
@@ -48,5 +48,10 @@ def logout():
     log_request()
     return jsonify(load_json("logout.json"))
 
+@app.route('/web_api/set-application-site', methods=['POST'])
+def set_application_site():
+    log_request()
+    return '', 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
